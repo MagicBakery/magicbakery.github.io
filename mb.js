763f2132baadba6c3a66f504e937be117e6c6ec3
Little Red Riding Hood <lrrh.bakery@gmail.com>
2023-07-23 12:25:39 -0700

    iFrame URL

--- mb.js
+++ mb.js
@@ -1,9 +1,17 @@
+function IFrameURLSet(el){
+  // 20230723: StarTree
+  var elIB = el.previousElementSibling;
+  var elIF = el.parentNode.nextElementSibling;
+  elIF.src = elIB.value;
+}
+
+
 function PanelAdd(el){
   // 20230722: StarTree
   var elMA = document.getElementById("MainArea");
+  var elNPT = document.getElementById("NewPanelTemplate");
   var elTemp = document.createElement("div");
-  elTemp.innerHTML = "<button class='mbbutton' onClick='PanelRemove(this)' style='float:right'>❌</button>Panel<br>";
-  elTemp.innerHTML += "<iframe src='https://panarcana.blogspot.com/p/viewer.html?id=P202303052122' title='Migration Node' style='border:none;width:100%;height:85vh' allow='clipboard-read; clipboard-write'></iframe>";
+  elTemp.innerHTML = elNPT.innerHTML;
   elTemp.classList.add('mbPanel');
 
   elMA.appendChild(elTemp);
@@ -15,5 +23,4 @@ function PanelAdd(el){
 function PanelRemove(el){
   // 20230722: StarTree
   el.parentNode.remove();
-
 }
