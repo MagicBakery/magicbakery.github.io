81886ec87db3c7414ffce309f37a2b4b21e2bcf2
Little Red Riding Hood <lrrh.bakery@gmail.com>
2023-07-22 22:19:13 -0700

    Clipboard read write

--- mb.js
+++ mb.js
@@ -3,7 +3,7 @@ function PanelAdd(el){
   var elMA = document.getElementById("MainArea");
   var elTemp = document.createElement("div");
   elTemp.innerHTML = "<button class='mbbutton' onClick='PanelRemove(this)' style='float:right'>❌</button>Panel<br>";
-  elTemp.innerHTML += "<iframe src='https://panarcana.blogspot.com/p/viewer.html?id=P202303052122' title='Migration Node' style='border:none;width:100%;height:85vh' ></iframe>";
+  elTemp.innerHTML += "<iframe src='https://panarcana.blogspot.com/p/viewer.html?id=P202303052122' title='Migration Node' style='border:none;width:100%;height:85vh' allow='clipboard-read; clipboard-write'></iframe>";
   elTemp.classList.add('mbPanel');
 
   elMA.appendChild(elTemp);
