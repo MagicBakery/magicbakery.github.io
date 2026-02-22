9625bbb390cf7e79d5b7b36f84f1c07198a4ac73
Little Red Riding Hood <lrrh.bakery@gmail.com>
2026-02-21 20:55:33 -0800

    .

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this every time you deploy new assets
-const CACHE_NAME = 'mt-app-20260221012700';
+const CACHE_NAME = 'mt-app-20260221205500';
 const ASSETS_TO_CACHE = [
   '/',           // The root URL
   '/mt.html',     // Your main app file
