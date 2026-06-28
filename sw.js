41c33eb2d6cd01c7fd5de420ecd0665285790582
Little Red Riding Hood <lrrh.bakery@gmail.com>
2026-06-28 01:35:56 -0700

    .

--- sw.js
+++ sw.js
@@ -36,9 +36,9 @@ self.addEventListener('activate', (event) => {
 // 3. Fetch Event: The "Offline Magic"
 self.addEventListener('fetch', (event) => {
   // CRITICAL FIX: Skip cross-origin extensions or internal schemes (like chrome-extension://)
-  if (!event.request.url.startsWith('http')) {
-    return; 
-  }
+  //if (!event.request.url.startsWith('http')) {
+//    return; 
+//  }
   // We only want to handle GET requests
   if (event.request.method !== 'GET') return;
 
